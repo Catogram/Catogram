964d6f2f45cb9a668cce150b6c261b6ca150125a
DrKLO <drklo.2kb@gmail.com>
2020-04-30 22:07:00 +0300

    6.1.1

--- TMessagesProj/src/main/java/org/telegram/ui/LoginActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/LoginActivity.java
@@ -1078,7 +1078,7 @@ public class LoginActivity extends BaseFragment {
         MessagesStorage.getInstance(currentAccount).putUsersAndChats(users, null, true, true);
         MessagesController.getInstance(currentAccount).putUser(res.user, false);
         ContactsController.getInstance(currentAccount).checkAppAccount();
-        MessagesController.getInstance(currentAccount).checkProxyInfo(true);
+        MessagesController.getInstance(currentAccount).checkPromoInfo(true);
         ConnectionsManager.getInstance(currentAccount).updateDcSettings();
         needFinishActivity(afterSignup);
     }
