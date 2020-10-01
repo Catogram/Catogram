56546ae0f24d67e889505681228c95a5e40f8770
DrKLO <drklo.dev@gmail.com>
2020-10-01 04:59:32 +0300

    Update to 7.1.1 (2094)

--- TMessagesProj/src/main/java/org/telegram/messenger/SendMessagesHelper.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/SendMessagesHelper.java
@@ -1222,6 +1222,7 @@ public class SendMessagesHelper extends BaseController implements NotificationCe
         }
         int lower_id = (int) peer;
         int sendResult = 0;
+        int myId = getUserConfig().getClientUserId();
         if (lower_id != 0) {
             final TLRPC.Peer peer_id = getMessagesController().getPeer((int) peer);
             boolean isMegagroup = false;
@@ -1230,6 +1231,7 @@ public class SendMessagesHelper extends BaseController implements NotificationCe
             boolean canSendMedia = true;
             boolean canSendPolls = true;
             boolean canSendPreview = true;
+            String rank = null;
             int linkedToGroup = 0;
             TLRPC.Chat chat;
             if (lower_id > 0) {
@@ -1265,7 +1267,6 @@ public class SendMessagesHelper extends BaseController implements NotificationCe
             LongSparseArray<TLRPC.Message> messagesByRandomIds = new LongSparseArray<>();
             TLRPC.InputPeer inputPeer = getMessagesController().getInputPeer(lower_id);
             long lastDialogId = 0;
-            int myId = getUserConfig().getClientUserId();
             final boolean toMyself = peer == myId;
             long lastGroupedId;
             for (int a = 0; a < messages.size(); a++) {
@@ -1295,7 +1296,7 @@ public class SendMessagesHelper extends BaseController implements NotificationCe
                 }
 
                 final TLRPC.Message newMsg = new TLRPC.TL_message();
-                boolean forwardFromSaved = msgObj.getDialogId() == myId && msgObj.isFromUser() && msgObj.messageOwner.from_id.user_id == getUserConfig().getClientUserId();
+                boolean forwardFromSaved = msgObj.getDialogId() == myId && msgObj.isFromUser() && msgObj.messageOwner.from_id.user_id == myId;
                 if (msgObj.isForwarded()) {
                     newMsg.fwd_from = new TLRPC.TL_messageFwdHeader();
                     if ((msgObj.messageOwner.fwd_from.flags & 1) != 0) {
@@ -1447,16 +1448,20 @@ public class SendMessagesHelper extends BaseController implements NotificationCe
                 if (peer_id.channel_id != 0 && !isMegagroup) {
                     if (isSignature) {
                         newMsg.from_id = new TLRPC.TL_peerUser();
-                        newMsg.from_id.user_id = getUserConfig().getClientUserId();
+                        newMsg.from_id.user_id = myId;
                     } else {
                         newMsg.from_id = peer_id;
                     }
                     newMsg.post = true;
                 } else if (ChatObject.shouldSendAnonymously(chat)) {
                     newMsg.from_id = peer_id;
+                    if (rank != null) {
+                        newMsg.post_author = rank;
+                        newMsg.flags |= 65536;
+                    }
                 } else {
                     newMsg.from_id = new TLRPC.TL_peerUser();
-                    newMsg.from_id.user_id = getUserConfig().getClientUserId();
+                    newMsg.from_id.user_id = myId;
                     newMsg.flags |= TLRPC.MESSAGE_FLAG_HAS_FROM_ID;
                 }
                 if (newMsg.random_id == 0) {
@@ -1610,6 +1615,7 @@ public class SendMessagesHelper extends BaseController implements NotificationCe
                                         final int oldId = newMsgObj1.id;
                                         final ArrayList<TLRPC.Message> sentMessages = new ArrayList<>();
                                         sentMessages.add(message);
+                                        msgObj1.messageOwner.post_author = message.post_author;
                                         updateMediaPaths(msgObj1, message, message.id, null, true);
                                         int existFlags = msgObj1.getMediaExistanceFlags();
                                         newMsgObj1.id = message.id;
@@ -2584,9 +2590,11 @@ public class SendMessagesHelper extends BaseController implements NotificationCe
         boolean isChannel = false;
         boolean forceNoSoundVideo = false;
         boolean anonymously = false;
+        String rank = null;
         int linkedToGroup = 0;
         TLRPC.EncryptedChat encryptedChat = null;
         TLRPC.InputPeer sendToPeer = lower_id != 0 ? getMessagesController().getInputPeer(lower_id) : null;
+        int myId = getUserConfig().getClientUserId();
         if (lower_id == 0) {
             encryptedChat = getMessagesController().getEncryptedChat(high_id);
             if (encryptedChat == null) {
@@ -2887,9 +2895,13 @@ public class SendMessagesHelper extends BaseController implements NotificationCe
                     newMsg.from_id.channel_id = sendToPeer.channel_id;
                 } else if (anonymously) {
                     newMsg.from_id = getMessagesController().getPeer(lower_id);
+                    if (rank != null) {
+                        newMsg.post_author = rank;
+                        newMsg.flags |= 65536;
+                    }
                 } else {
                     newMsg.from_id = new TLRPC.TL_peerUser();
-                    newMsg.from_id.user_id = getUserConfig().getClientUserId();
+                    newMsg.from_id.user_id = myId;
                     newMsg.flags |= TLRPC.MESSAGE_FLAG_HAS_FROM_ID;
                 }
                 getUserConfig().saveConfig(false);
@@ -2926,7 +2938,7 @@ public class SendMessagesHelper extends BaseController implements NotificationCe
                             newMsg.post = true;
                             if (chat.signatures) {
                                 newMsg.from_id = new TLRPC.TL_peerUser();
-                                newMsg.from_id.user_id = getUserConfig().getClientUserId();
+                                newMsg.from_id.user_id = myId;
                             }
                         }
                     }
@@ -2976,7 +2988,7 @@ public class SendMessagesHelper extends BaseController implements NotificationCe
                 }
             } else {
                 newMsg.peer_id = new TLRPC.TL_peerUser();
-                if (encryptedChat.participant_id == getUserConfig().getClientUserId()) {
+                if (encryptedChat.participant_id == myId) {
                     newMsg.peer_id.user_id = encryptedChat.admin_id;
                 } else {
                     newMsg.peer_id.user_id = encryptedChat.participant_id;
@@ -4787,6 +4799,7 @@ public class SendMessagesHelper extends BaseController implements NotificationCe
                                     }
                                     message.unread = value < message.id;
                                 }
+                                msgObj.messageOwner.post_author = message.post_author;
                                 updateMediaPaths(msgObj, message, message.id, originalPath, false);
                                 existFlags = msgObj.getMediaExistanceFlags();
                                 newMsgObj.id = message.id;
--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -10877,6 +10877,9 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                     if (messagesDict[loadIndex].indexOfKey(messageId) >= 0) {
                         continue;
                     }
+                    if (threadMessageId != 0 && obj.messageOwner instanceof TLRPC.TL_messageEmpty) {
+                        continue;
+                    }
                     if (currentEncryptedChat != null && obj.messageOwner.stickerVerified == 0) {
                         getMediaDataController().verifyAnimatedStickerMessage(obj.messageOwner);
                     }
@@ -11662,6 +11665,9 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                         if (action instanceof TLRPC.TL_messageActionChannelMigrateFrom) {
                             continue;
                         }
+                        if (threadMessageId != 0 && obj.messageOwner instanceof TLRPC.TL_messageEmpty) {
+                            continue;
+                        }
                         if (threadMessageObject != null && obj.isReply()) {
                             int mid = obj.getReplyAnyMsgId();
                             if (threadMessageObject.getId() == mid) {
@@ -11679,10 +11685,12 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                             last_message_id = Math.min(last_message_id, messageId);
                         }
 
-                        if (obj.messageOwner.mentioned && obj.isContentUnread()) {
-                            newMentionsCount++;
+                        if (threadMessageId == 0) {
+                            if (obj.messageOwner.mentioned && obj.isContentUnread()) {
+                                newMentionsCount++;
+                            }
+                            newUnreadMessageCount++;
                         }
-                        newUnreadMessageCount++;
                         if (obj.type == 10 || obj.type == 11) {
                             updateChat = true;
                         }
@@ -11758,6 +11766,9 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                             continue;
                         }
                         if (action instanceof TLRPC.TL_messageActionChannelMigrateFrom) {
+                            continue;
+                        }
+                        if (threadMessageId != 0 && obj.messageOwner instanceof TLRPC.TL_messageEmpty) {
                             continue;
                         }
                         if (threadMessageObject != null && threadMessageObject.messageOwner.replies != null && obj.isReply()) {
@@ -11960,10 +11971,12 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                                 changeBoundAnimator.start();
                             }
                         }
-                        if (!obj.isOut() && obj.messageOwner.mentioned && obj.isContentUnread()) {
-                            newMentionsCount++;
+                        if (threadMessageId == 0) {
+                            if (!obj.isOut() && obj.messageOwner.mentioned && obj.isContentUnread()) {
+                                newMentionsCount++;
+                            }
+                            newUnreadMessageCount++;
                         }
-                        newUnreadMessageCount++;
                         if (obj.type == 10 || obj.type == 11) {
                             updateChat = true;
                         }
@@ -12532,11 +12545,13 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                     for (int a = 0, N = removedIndexes.size(); a < N; a++) {
                         chatAdapter.notifyItemRemoved(removedIndexes.get(a));
                     }
-                    removeUnreadPlane(false);
+                    if (!isThreadChat() || messages.size() <= 3) {
+                        removeUnreadPlane(false);
+                    }
                     chatAdapter.notifyItemRangeChanged(chatAdapter.messagesStartRow, messages.size());
                 }
                 updateVisibleRows();
-            } else {
+            } else if (threadMessageId == 0) {
                 first_unread_id = 0;
                 last_message_id = 0;
                 createUnreadMessageAfterId = 0;
@@ -12841,7 +12856,15 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
         } else if (id == NotificationCenter.removeAllMessagesFromDialog) {
             long did = (Long) args[0];
             if (dialog_id == did) {
-                clearHistory((Boolean) args[1]);
+                if (threadMessageId != 0) {
+                    if (forwardEndReached[0]) {
+                        forwardEndReached[0] = false;
+                        chatAdapter.notifyItemInserted(0);
+                    }
+                    MessagesController.getInstance(currentAccount).addToViewsQueue(threadMessageObject);
+                } else {
+                    clearHistory((Boolean) args[1]);
+                }
             }
         } else if (id == NotificationCenter.screenshotTook) {
             updateInformationForScreenshotDetector();
@@ -17062,7 +17085,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                 break;
             }
             case 27: {
-                openDiscussionMessageChat(currentChat.id, selectedObject.getId(), 0, -1, 0, null);
+                openDiscussionMessageChat(currentChat.id, null, selectedObject.getId(), 0, -1, 0, null);
                 break;
             }
             case 100: {
@@ -17608,7 +17631,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
     private boolean savedNoHistory;
     private boolean savedNoDiscussion;
 
-    private void processLoadedDiscussionMessage(boolean noDiscussion, TLRPC.TL_messages_discussionMessage discussionMessage, boolean noHistory, TLRPC.messages_Messages history, int maxReadId, MessageObject fallbackMessage, Runnable progressRunnable, TLRPC.TL_messages_getDiscussionMessage req, TLRPC.Chat originalChat, int highlightMsgId) {
+    private void processLoadedDiscussionMessage(boolean noDiscussion, TLRPC.TL_messages_discussionMessage discussionMessage, boolean noHistory, TLRPC.messages_Messages history, int maxReadId, MessageObject fallbackMessage, Runnable progressRunnable, TLRPC.TL_messages_getDiscussionMessage req, TLRPC.Chat originalChat, int highlightMsgId, MessageObject originalMessage) {
         if (!noDiscussion && discussionMessage == null || noDiscussion || !noHistory && history == null) {
             return;
         }
@@ -17637,6 +17660,9 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
             chatActivity.setThreadMessages(arrayList, originalChat, req.msg_id, discussionMessage.read_inbox_max_id, discussionMessage.read_outbox_max_id);
             if (highlightMsgId != 0) {
                 chatActivity.highlightMessageId = highlightMsgId;
+            }
+            if (originalMessage != null && originalMessage.messageOwner.replies != null && chatActivity.threadMessageObject.messageOwner.replies != null) {
+                originalMessage.messageOwner.replies.replies = chatActivity.threadMessageObject.messageOwner.replies.replies;
             }
             presentFragment(chatActivity);
             chatOpened = true;
@@ -17669,7 +17695,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
         }
     }
 
-    private void openDiscussionMessageChat(int chatId, int messageId, int linkedChatId, int maxReadId, int highlightMsgId, MessageObject fallbackMessage) {
+    private void openDiscussionMessageChat(int chatId, MessageObject originalMessage, int messageId, int linkedChatId, int maxReadId, int highlightMsgId, MessageObject fallbackMessage) {
         TLRPC.Chat chat = getMessagesController().getChat(chatId);
         TLRPC.TL_messages_getDiscussionMessage req = new TLRPC.TL_messages_getDiscussionMessage();
         req.peer = MessagesController.getInputPeer(chat);
@@ -17722,7 +17748,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                 } else {
                     savedNoHistory = true;
                 }
-                processLoadedDiscussionMessage(savedNoDiscussion, savedDiscussionMessage, savedNoHistory, savedHistory, maxReadId, fallbackMessage, progressRunnable, req, chat, highlightMsgId);
+                processLoadedDiscussionMessage(savedNoDiscussion, savedDiscussionMessage, savedNoHistory, savedHistory, maxReadId, fallbackMessage, progressRunnable, req, chat, highlightMsgId, originalMessage);
             }));
             getConnectionsManager().bindRequestToGuid(commentMessagesRequestId, classGuid);
         } else {
@@ -17741,7 +17767,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
             } else {
                 savedNoDiscussion = true;
             }
-            processLoadedDiscussionMessage(savedNoDiscussion, savedDiscussionMessage, savedNoHistory, savedHistory, maxReadId, fallbackMessage, progressRunnable, req, chat, highlightMsgId);
+            processLoadedDiscussionMessage(savedNoDiscussion, savedDiscussionMessage, savedNoHistory, savedHistory, maxReadId, fallbackMessage, progressRunnable, req, chat, highlightMsgId, originalMessage);
         }));
         getConnectionsManager().bindRequestToGuid(commentRequestId, classGuid);
         AndroidUtilities.runOnUIThread(progressRunnable, 500);
@@ -18362,7 +18388,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                         MessageObject messageObject = cell.getMessageObject();
                         if ((UserObject.isReplyUser(currentUser) || UserObject.isUserSelf(currentUser)) && messageObject.messageOwner.fwd_from.saved_from_peer != null) {
                             if (UserObject.isReplyUser(currentUser) && messageObject.messageOwner.reply_to != null && messageObject.messageOwner.reply_to.reply_to_top_id != 0) {
-                                openDiscussionMessageChat(messageObject.messageOwner.reply_to.reply_to_peer_id.channel_id, messageObject.messageOwner.reply_to.reply_to_top_id, 0, -1, messageObject.messageOwner.fwd_from.saved_from_msg_id, messageObject);
+                                openDiscussionMessageChat(messageObject.messageOwner.reply_to.reply_to_peer_id.channel_id, null, messageObject.messageOwner.reply_to.reply_to_top_id, 0, -1, messageObject.messageOwner.fwd_from.saved_from_msg_id, messageObject);
                             } else {
                                 openOriginalReplyChat(messageObject);
                             }
@@ -18419,7 +18445,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                             processRowSelect(cell, true, touchX, touchY);
                             return;
                         }
-                        openChannel(cell, chat, postId);
+                        openChat(cell, chat, postId);
                     }
 
                     @Override
@@ -18511,7 +18537,11 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                     @Override
                     public boolean didLongPressChannelAvatar(ChatMessageCell cell, TLRPC.Chat chat, int postId, float touchX, float touchY) {
                         if (isAvatarPreviewerEnabled()) {
-                            final AvatarPreviewer.MenuItem[] menuItems = {AvatarPreviewer.MenuItem.OPEN_PROFILE, AvatarPreviewer.MenuItem.OPEN_CHANNEL};
+                            AvatarPreviewer.MenuItem[] menuItems = {AvatarPreviewer.MenuItem.OPEN_PROFILE};
+                            if (currentChat == null || currentChat.id != chat.id || isThreadChat()) {
+                                menuItems = Arrays.copyOf(menuItems, 2);
+                                menuItems[1] = chat.broadcast ? AvatarPreviewer.MenuItem.OPEN_CHANNEL : AvatarPreviewer.MenuItem.OPEN_GROUP;
+                            }
                             final TLRPC.ChatFull chatFull = getMessagesController().getChatFull(chat.id);
                             final AvatarPreviewer.Data data;
                             if (chatFull != null) {
@@ -18525,8 +18555,9 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                                         case OPEN_PROFILE:
                                             openProfile(chat);
                                             break;
+                                        case OPEN_GROUP:
                                         case OPEN_CHANNEL:
-                                            openChannel(cell, chat, 0);
+                                            openChat(cell, chat, 0);
                                             break;
                                     }
                                 });
@@ -18565,7 +18596,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                         }
                     }
 
-                    private void openChannel(ChatMessageCell cell, TLRPC.Chat chat, int postId) {
+                    private void openChat(ChatMessageCell cell, TLRPC.Chat chat, int postId) {
                         if (currentChat == null || chat.id != currentChat.id || isThreadChat()) {
                             Bundle args = new Bundle();
                             args.putInt("chat_id", chat.id);
@@ -18853,7 +18884,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                             maxReadId = -1;
                             linkedChatId = 0;
                         }
-                        openDiscussionMessageChat(currentChat.id, message.getId(), linkedChatId, maxReadId, 0, null);
+                        openDiscussionMessageChat(currentChat.id, message, message.getId(), linkedChatId, maxReadId, 0, null);
                     }
 
                     @Override
@@ -19062,7 +19093,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                                 if (pinnedBottom && fromId < 0 && currentChat.megagroup) {
                                     pinnedBottom = false;
                                 }
-                            } else if (UserObject.isUserSelf(currentUser)) {
+                            } else if (UserObject.isUserSelf(currentUser) || UserObject.isReplyUser(currentUser)) {
                                 if (message.isPrivateForward() || nextMessage.isPrivateForward()) {
                                     pinnedBottom = false;
                                 } else {
@@ -19081,7 +19112,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                                 if (pinnedTop && fromId < 0 && currentChat.megagroup) {
                                     pinnedTop = false;
                                 }
-                            } else if (UserObject.isUserSelf(currentUser)) {
+                            } else if (UserObject.isUserSelf(currentUser) || UserObject.isReplyUser(currentUser)) {
                                 if (message.isPrivateForward() || prevMessage.isPrivateForward()) {
                                     pinnedTop = false;
                                 } else {
@@ -19350,7 +19381,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                     View child = chatListView.getChildAt(a);
                     if (child instanceof ChatMessageCell) {
                         ChatMessageCell cell = (ChatMessageCell) child;
-                        if (cell.getMessageObject() == messageObject) {
+                        if (cell.getMessageObject() == messageObject && !cell.isAdminLayoutChanged()) {
                             cell.setMessageObject(messageObject, cell.getCurrentMessagesGroup(), cell.isPinnedBottom(), cell.isPinnedTop());
                             return cell;
                         }
@@ -19530,7 +19561,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
             int threadId = Utilities.parseInt(data.getQueryParameter("thread"));
             if (channelId == currentChat.id && messageId != 0) {
                 if (threadId != 0) {
-                    openDiscussionMessageChat(currentChat.id, threadId, 0, -1, 0, null);
+                    openDiscussionMessageChat(currentChat.id, null, threadId, 0, -1, 0, null);
                 } else {
                     showScrollToMessageError = true;
                     scrollToMessageId(messageId, fromMessageId, true, 0, false);
