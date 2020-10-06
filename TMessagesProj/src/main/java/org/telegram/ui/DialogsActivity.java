d4499708ac8eeb4070a4a88b8230a67ae30a16a6
Nick C <ctwoon@pixelexperience.org>
2020-10-06 16:54:46 +0000

    [VK-UI] change actionbartitle

--- TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
@@ -1794,11 +1794,7 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
             if (folderId != 0) {
                 actionBar.setTitle(LocaleController.getString("ArchivedChats", R.string.ArchivedChats));
             } else {
-                if (BuildVars.DEBUG_VERSION) {
-                    actionBar.setTitle("Telegram Beta");
-                } else {
-                    actionBar.setTitle("Catogram");
-                }
+                    actionBar.setTitle(LocaleController.getString("ActionBarTitle", R.string.ActionBarTitle));
             }
             if (folderId == 0) {
                 actionBar.setSupportsHolidayImage(true);
