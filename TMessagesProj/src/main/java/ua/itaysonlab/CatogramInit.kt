77893a013f1aa506a091cf6c1b16f3df2fa91c2c
iTaysonLab <itaysontesterlab@gmail.com>
2020-10-30 20:55:27 +0200

    Local AirUI

--- TMessagesProj/src/main/java/ua/itaysonlab/CatogramInit.kt
+++ TMessagesProj/src/main/java/ua/itaysonlab/CatogramInit.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import bruhcollective.itaysonlab.airui.core.externalapi.ExternalApiProviders
 import bruhcollective.itaysonlab.airui.core.externalapi.providers.AccentColorProvider
 import bruhcollective.itaysonlab.airui.core.externalapi.providers.DarkModeProvider
-import bruhcollective.itaysonlab.airui.core.externalapi.providers.PaletteProvider
 import org.telegram.ui.ActionBar.Theme
 
 object CatogramInit {
@@ -18,16 +17,6 @@ object CatogramInit {
         ExternalApiProviders.darkModeProvider = object: DarkModeProvider() {
             override fun provide(ctx: Context): Boolean {
                 return Theme.isCurrentThemeNight()
-            }
-        }
-
-        ExternalApiProviders.paletteProvider = object: PaletteProvider() {
-            override fun getColor(ctx: Context, color: Color): Int {
-                return Theme.getColor(when (color) {
-                    Color.TEXT -> Theme.key_windowBackgroundWhiteBlackText
-                    Color.TEXT_SECONDARY -> Theme.key_windowBackgroundWhiteGrayText
-                    Color.BACKGROUND -> Theme.key_windowBackgroundWhite
-                })
             }
         }
     }
