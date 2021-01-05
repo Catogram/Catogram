5baa5d2c2ef80436229c7e84cd3b699547da9e92
Nick C <ctwoon@suicide.today>
2021-01-05 21:38:36 +0300

    Fix typo in build script

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ sed -i s/CATOGRAM_ALIAS/${KEY_ALIAS}/ gradle.properties
 sed -i s/CATOGRAM_PASSWORD/${KEY_PASS}/ gradle.properties
 sed -i s/CATOGRAM_STORE_PASSWORD/${STORE_PASS}/ gradle.properties
 sed -i s/CATOGRAM_APP_ID/${API_KEY}/ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
-sed -i s/CATOGRAM_API_ID/${API_HASH}/ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+sed -i s/CATOGRAM_API_HASH/${API_HASH}/ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
 #-------------------#
 ./gradlew assembleAfatRelease
 #-------------------#
