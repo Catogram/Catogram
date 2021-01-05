2952da996d55b2322c8daf69058253f1e1502643
Nick C <ctwoon@suicide.today>
2021-01-05 15:25:08 +0300

    build afat and upload output to https://x0.at

--- build.sh
+++ build.sh
@@ -7,12 +7,11 @@ sed -i s/CATOGRAM_ALIAS/${KEY_ALIAS}/
 sed -i s/CATOGRAM_PASSWORD/${KEY_PASS}/
 sed -i s/CATOGRAM_STORE_PASSWORD/${STORE_PASS}/
 #-------------------#
-./gradlew assembleRelease
+./gradlew assembleAfatRelease
 #-------------------#
-cd TMessagesProj/build/outputs/apk/*/release
-curl --progress-bar -F document=@"app.apk" "https://api.telegram.org/bot${TG_BOT_KEY}/sendDocument" \
--F chat_id="-1001293922100"  \
--F "disable_web_page_preview=true" \
--F "parse_mode=html" \
--F caption="Build done"
+cd TMessagesProj/build/outputs/apk/afat/release
+curl -s -X POST "https://api.telegram.org/bot${TG_BOT_KEY}/sendMessage" -d chat_id="-1001293922100" \
+  -d "disable_web_page_preview=true" \
+  -d "parse_mode=markdown" \
+  -d text="$(curl -F "file=@app.apk" https://x0.at/ | cat)"
 #-------------------# 
