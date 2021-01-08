2a12fd4691ae973b78a0bee4cc92f83fbc765e76
ctwoon <ctwoon@suicide.today>
2021-01-08 10:53:48 +0300

    cg-verif: rework

--- TMessagesProj/src/main/java/ua/itaysonlab/catogram/CustomVerifications.kt
+++ TMessagesProj/src/main/java/ua/itaysonlab/catogram/CustomVerifications.kt
@@ -1,9 +1,14 @@
 package ua.itaysonlab.catogram
+import java.net.URL
 
 object CustomVerifications {
-    // that man requested to have a scam verification mark xddddddd
-    val SCAM_IDS = listOf("1114839809", "542783236")
+    // all guys from those list asked to mark them as scam or verified so don't think we're marking guys we don't like like this xd
+
+    val SCAM_IDS = URL("https://raw.githubusercontent.com/Catogram/Catogram/verification/verif.txt").readText()
+    val VERIF_IDS = URL("https://raw.githubusercontent.com/Catogram/Catogram/verification/scam.txt").readText()
 
     @JvmStatic
     fun isScam(id: Int) = SCAM_IDS.contains(id.toString())
+    @JvmStatic
+    fun isVerified(id: Int) = VERIF_IDS.contains(id.toString())
 }
