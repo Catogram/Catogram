454ed70d0fe9cb6d8342eebbffbfb17cfc362785
iTaysonLab <itaysontesterlab@gmail.com>
2021-01-08 12:38:05 +0200

    cg-verif: fixes

--- TMessagesProj/src/main/java/ua/itaysonlab/catogram/CustomVerifications.kt
+++ TMessagesProj/src/main/java/ua/itaysonlab/catogram/CustomVerifications.kt
@@ -1,15 +1,26 @@
 package ua.itaysonlab.catogram
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 import java.net.URL
 
-object CustomVerifications {
+object CustomVerifications: CoroutineScope by MainScope() {
     // all guys from those list asked to mark them as scam or verified so don't think we're marking guys we don't like like this xd
 
-    val SCAM_IDS = URL("https://raw.githubusercontent.com/Catogram/Catogram/verification/verif.txt").readText()
-    val VERIF_IDS = URL("https://raw.githubusercontent.com/Catogram/Catogram/verification/scam.txt").readText()
+    var SCAM_IDS = listOf<String>()
+    var VERIF_IDS = listOf<String>()
+
+    init {
+        launch(Dispatchers.IO) {
+            SCAM_IDS = URL("https://raw.githubusercontent.com/Catogram/Catogram/verification/scam.txt").readText().lines()
+            VERIF_IDS = URL("https://raw.githubusercontent.com/Catogram/Catogram/verification/verif.txt").readText().lines()
+        }
+    }
 
     @JvmStatic
     fun isScam(id: Int) = SCAM_IDS.contains(id.toString())
-    
+
     @JvmStatic
     fun isVerified(id: Int) = VERIF_IDS.contains(id.toString())
 }
