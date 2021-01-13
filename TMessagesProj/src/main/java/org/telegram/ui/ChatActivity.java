90e64f45ee54e42bcec0bf7b4e97413dfcff5a35
pqhaz <me@pqhaz.dev>
2021-01-13 21:50:13 +0300

    ChatActivity: Remove unused import
    Signed-off-by: pqhaz <me@pqhaz.dev>

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -26,7 +26,6 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Outline;
