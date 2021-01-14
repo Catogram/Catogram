91910f0fb71144a287697a4b20c08dbfeb42a563
pqhaz <me@pqhaz.dev>
2021-01-14 17:44:39 +0300

    StickerDownloader fixes
    Signed-off-by: pqhaz <me@pqhaz.dev>

--- TMessagesProj/src/main/java/ua/itaysonlab/catogram/stickers/StickerDownloader.kt
+++ TMessagesProj/src/main/java/ua/itaysonlab/catogram/stickers/StickerDownloader.kt
@@ -3,24 +3,22 @@ package ua.itaysonlab.catogram.stickers
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import org.telegram.messenger.FileLoader
-import org.telegram.messenger.MediaController
 import org.telegram.messenger.MessageObject
 import java.io.File
-import java.lang.Exception
 
 
 class StickerDownloader(val message: MessageObject)  {
     fun download(): Pair<String, String> {
-        val stickerPath = FileLoader.getPathToMessage(message.messageOwner).toString()
-        val stickerName = message.fileName.replace(".webp", ".png")
-        val stickerPngPath = stickerPath.replace(".webp", ".png")
-        val stickerBitmap = BitmapFactory.decodeFile(stickerPath);
+        val file = FileLoader.getPathToAttach(message.document, true)
+        val stickerPngName = file.name.toString().replace(".webp", ".png")
+        val stickerPngPath = file.path.toString().replace(".webp", ".png")
+        val stickerBitmap = BitmapFactory.decodeStream(file.inputStream())
 
         File(stickerPngPath).outputStream().use {out ->
             stickerBitmap.compress(Bitmap.CompressFormat.PNG, 100, out)
         }
         stickerBitmap.recycle()
 
-        return Pair(stickerName, stickerPngPath)
+        return Pair(stickerPngName, stickerPngPath)
     }
 }
