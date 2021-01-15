d682abd4a0f66c1aee4f084667f2ec0fdf8fe099
ctwoon <ctwoon@suicide.today>
2021-01-15 22:04:11 +0300

    donate: add controversive methods

--- TMessagesProj/src/main/java/ua/itaysonlab/catogram/preferences/GPDonateEntry.kt
+++ TMessagesProj/src/main/java/ua/itaysonlab/catogram/preferences/GPDonateEntry.kt
@@ -4,14 +4,19 @@ import org.telegram.messenger.LocaleController
 import org.telegram.messenger.R
 import org.telegram.ui.ActionBar.BaseFragment
 import org.telegram.ui.LaunchActivity
+import org.telegram.messenger.AndroidUtilities
 import ua.itaysonlab.catogram.preferences.ktx.category
 import ua.itaysonlab.catogram.preferences.ktx.textDetail
 import ua.itaysonlab.catogram.preferences.ktx.textIcon
 import ua.itaysonlab.catogram.preferences.ktx.tgKitScreen
 import ua.itaysonlab.tgkit.preference.types.TGKitTextIconRow
+import ua.itaysonlab.catogram.CGControversive
+import android.content.Intent
+import android.net.Uri
 
 class GPDonateEntry : BasePreferencesEntry {
     override fun getPreferences(bf: BaseFragment) = tgKitScreen(LocaleController.getString("CG_GooglePlay_Donate", R.string.CG_GooglePlay_Donate)) {
+        if (!CGControversive.isControversiveFeaturesEnabled()) {
         category(LocaleController.getString("CG_GooglePlay_Info", R.string.CG_GooglePlay_Info)) {
             textDetail {
                 title = LocaleController.getString("CG_GooglePlay_Info", R.string.CG_GooglePlay_Info)
@@ -50,5 +55,44 @@ class GPDonateEntry : BasePreferencesEntry {
                 }
             }
         }
+    }
+    else {
+        category(LocaleController.getString("CG_GooglePlay_Info", R.string.CG_GooglePlay_Info)) {
+            textDetail {
+                title = LocaleController.getString("CG_GooglePlay_Info", R.string.CG_GooglePlay_Info)
+                detail = LocaleController.getString("CG_Donate_Desc", R.string.CG_Donate_Desc)
+            }
+        }
+        category(LocaleController.getString("CG_Donate_Method", R.string.CG_Donate_Method)) {
+            textIcon {
+                title = "QIWI"
+                divider = true
+
+                listener = TGKitTextIconRow.TGTIListener {
+                    val openURL = Intent(android.content.Intent.ACTION_VIEW)
+                    openURL.data = Uri.parse("https://qiwi.com/n/hackerone228/")
+                    bf.parentActivity.startActivity(openURL)
+                }
+            }
+            textIcon {
+                title = "DonatePay"
+                divider = true
+
+                listener = TGKitTextIconRow.TGTIListener {
+                    val openURL = Intent(android.content.Intent.ACTION_VIEW)
+                    openURL.data = Uri.parse("https://new.donatepay.ru/@ctwoon")
+                    bf.parentActivity.startActivity(openURL)
+                }
+            }
+            textIcon {
+                title = LocaleController.getString("CG_Mono_Desc", R.string.CG_Mono_Desc)
+                divider = true
+
+                listener = TGKitTextIconRow.TGTIListener {
+                    AndroidUtilities.addToClipboard("5375414117420108")
+                }
+            }
+        }
+    }
     }
 }
