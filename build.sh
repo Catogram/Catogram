28680ee43cc655825ea0c7cfc670563f0cfaa19e
ctwoon <ctwoon@suicide.today>
2021-03-31 13:36:08 +0300

    drone: silence

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ sed -i s/CATOGRAM_STORE_PASSWORD/${STORE_PASS}/ gradle.properties
 sed -i s/CATOGRAM_APP_ID/${API_KEY}/ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
 sed -i s/CATOGRAM_API_HASH/${API_HASH}/ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
 #-------------------#
-./gradlew assembleAfatRelease
+./gradlew assembleAfatRelease | > /dev/null
 #-------------------#
 cd TMessagesProj/build/outputs/apk/afat/release
-curl -F chat_id="-1001293922100" -F document=@app.apk -F caption="Head: `$(git log --pretty=format:'%h' -n 1)`" -F parse_mode=markdown https://bot.pokurt.me/bot${TG_BOT_KEY}/sendDocument
+curl -F chat_id="-1001293922100" -F document=@app.apk -F caption="Head: $(git log --pretty=format:'%h' -n 1)" -F parse_mode=markdown https://bot.pokurt.me/bot${TG_BOT_KEY}/sendDocument
 #-------------------# 
