2f8f7b8e1362e7819984762d4d272d81421ad640
rf0x3d <48347444+rfoxxxy@users.noreply.github.com>
2021-04-25 21:27:54 +0300

    fixed image caption and polls translation

--- TMessagesProj/src/main/java/ua/itaysonlab/catogram/translate/TranslateAPI.kt
+++ TMessagesProj/src/main/java/ua/itaysonlab/catogram/translate/TranslateAPI.kt
@@ -12,10 +12,9 @@ object TranslateAPI {
 
     fun extractMessageText(msg: MessageObject): String {
         return when {
-            msg.isPhoto -> msg.messageOwner.media.photo.caption
             msg.isPoll -> {
                 val poll = (msg.messageOwner.media as TLRPC.TL_messageMediaPoll).poll
-                "$poll\n\n${poll.answers.joinToString("\n") { "- ${it.text}" }}"
+                "${poll.question}\n\n${poll.answers.joinToString("\n") { "- ${it.text}" }}"
             }
             else -> if (msg.caption != null) msg.caption.toString() else msg.messageText.toString()
         }
