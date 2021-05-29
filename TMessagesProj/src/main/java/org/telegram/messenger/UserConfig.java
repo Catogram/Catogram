a753c92ef08b53e345c85b2ffd522fec772b0659
Nick C <ctwoon@suicide.today>
2021-05-29 19:22:46 +0300

    Revert "decrease max accounts to 4"
    This reverts commit cf0793213211656359307dc35480c0e621927f4b.

--- TMessagesProj/src/main/java/org/telegram/messenger/UserConfig.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/UserConfig.java
@@ -20,10 +20,12 @@ import org.telegram.tgnet.TLRPC;
 import java.io.File;
 import java.util.Arrays;
 
+import ua.itaysonlab.extras.CatogramExtras;
+
 public class UserConfig extends BaseController {
 
     public static int selectedAccount;
-    public final static int MAX_ACCOUNT_COUNT = 4;
+    public final static int MAX_ACCOUNT_COUNT = CatogramExtras.MAX_ACCOUNT_COUNT;
 
     private final Object sync = new Object();
     private boolean configLoaded;
