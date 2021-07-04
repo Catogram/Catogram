a678f930e0b50e4ca215b091a72d7c82813168af
Nick C <pokemonnekitgod@gmail.com>
2021-07-04 13:31:58 +0300

    adapt OTA for pling & improve

--- TMessagesProj/src/main/java/ua/itaysonlab/catogram/OTA.kt
+++ TMessagesProj/src/main/java/ua/itaysonlab/catogram/OTA.kt
@@ -5,14 +5,11 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import android.net.ConnectivityManager
 import android.net.Uri
 import android.os.Build
-import android.os.Environment
 import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
-import androidx.core.content.ContextCompat.getSystemService
 import kotlinx.coroutines.*
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -20,7 +17,6 @@ import org.json.JSONObject
 import org.telegram.messenger.LocaleController
 import org.telegram.messenger.R
 import ua.itaysonlab.extras.CatogramExtras
-
 
 object OTA : CoroutineScope by MainScope() {
 
@@ -39,14 +35,14 @@ object OTA : CoroutineScope by MainScope() {
         launch(handler) {
             try {
                 val request: Request =
-                        Request.Builder().url("https://api.github.com/repos/catogram/catogram/releases/latest").build()
+                        Request.Builder().url("https://ctwoon.eu/catogram.json").build()
                 withContext(Dispatchers.IO) {
                     val response = OkHttpClient().newCall(request).execute()
                     parseddString = response.body!!.string()
                     val parsedString = JSONObject(parseddString)
-                    if (parsedString.getString("name") != CatogramExtras.CG_VERSION) {
-                        version = parsedString.getString("name")
-                        changelog = parsedString.getString("body")
+                    if (parsedString.getString("version") != CatogramExtras.CG_VERSION) {
+                        version = parsedString.getString("version")
+                        changelog = parsedString.getString("changelog")
                         needDownload = true
                     } else needDownload = false
                 }
@@ -65,10 +61,6 @@ object OTA : CoroutineScope by MainScope() {
                     "action_download" -> {
                         downloadApk(context!!)
                     }
-
-                    "action_changelog" -> {
-                        showAlert(context!!)
-                    }
                 }
             }
         }
@@ -78,17 +70,7 @@ object OTA : CoroutineScope by MainScope() {
         } catch (e: Exception) {
         }
 
-        handler = CoroutineExceptionHandler { _, exception ->
-            if (!exception.message?.contains("Unable to resolve host")!!) {
-                val builder = org.telegram.ui.ActionBar.AlertDialog.Builder(context)
-                builder.setTitle(LocaleController.getString("ErrorOccurred", R.string.ErrorOccurred))
-                        .setMessage(exception.message)
-                        .setPositiveButton("OK") { dialog, _ ->
-                            dialog.cancel()
-                        }
-                builder.show()
-            }
-        }
+        handler = CoroutineExceptionHandler { _, _ -> }
 
         checkBS { needDownload ->
             if (needDownload && b) {
@@ -113,14 +95,6 @@ object OTA : CoroutineScope by MainScope() {
                         intentDownload, PendingIntent.FLAG_UPDATE_CURRENT
                 )
 
-                val intentChangelog = Intent(context, NotificationActionService::class.java)
-                        .setAction("action_changelog")
-
-                val pendingIntentChangelog = PendingIntent.getBroadcast(
-                        context, 0,
-                        intentChangelog, PendingIntent.FLAG_UPDATE_CURRENT
-                )
-
                 val notification: Notification = NotificationCompat.Builder(context, "channel01")
                         .setSmallIcon(R.drawable.cg_notification)
                         .setContentTitle(LocaleController.getString("CG_Found", R.string.CG_Found))
@@ -128,7 +102,8 @@ object OTA : CoroutineScope by MainScope() {
                         .setDefaults(Notification.DEFAULT_ALL)
                         .setPriority(NotificationCompat.PRIORITY_HIGH)
                         .addAction(R.drawable.download_outline_28, LocaleController.getString("CG_Download", R.string.CG_Download), pendingIntentDownload)
-                        .addAction(R.drawable.download_outline_28, LocaleController.getString("CG_Changelog", R.string.CG_Changelog), pendingIntentChangelog)
+                        .setStyle(NotificationCompat.BigTextStyle()
+                            .bigText(changelog))
                         .build()
 
                 val notificationManager = NotificationManagerCompat.from(context)
@@ -153,18 +128,12 @@ object OTA : CoroutineScope by MainScope() {
                 }
         builder.show()
     }
-
     fun downloadApk(context: Context) {
         val notificationManager = NotificationManagerCompat.from(context)
         notificationManager.cancel(1337)
-        val request = DownloadManager.Request(Uri.parse("https://github.com/catogram/catogram/releases/latest/download/app.apk"))
+        val openURL = Intent(Intent.ACTION_VIEW)
+        openURL.data = Uri.parse("https://www.pling.com/p/1548633/")
+        context.startActivity(openURL)
 
-        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE or DownloadManager.Request.NETWORK_WIFI)
-        request.setTitle("Catogram v$version")
-        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-        request.setDestinationInExternalFilesDir(context, Environment.DIRECTORY_DOWNLOADS, "ota.apk")
-
-        val manager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-        manager.enqueue(request)
     }
 }
