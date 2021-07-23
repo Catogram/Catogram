a330ee57e553688fff0b7c1a130648ed973f9ebb
Nick C <pokemonnekitgod@gmail.com>
2021-07-23 16:20:36 +0300

    initial support for dynamic (monet) themes

---
+++ TMessagesProj/src/main/java/ua/itaysonlab/catogram/MonetHelper.kt
@@ -0,0 +1,35 @@
+package ua.itaysonlab.catogram
+
+import android.R
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
+import androidx.annotation.AttrRes
+import androidx.annotation.ColorInt
+import androidx.annotation.RequiresApi
+import androidx.appcompat.view.ContextThemeWrapper
+import com.google.android.material.color.MaterialColors
+import org.telegram.ui.ActionBar.Theme
+
+object MonetHelper {
+
+    @ColorInt
+    @JvmStatic
+    fun getAccentColor(ctx: Context): Int {
+        return reqAttrFromDevice(ctx, R.attr.colorAccent)
+    }
+
+    @ColorInt
+    @JvmStatic
+    fun getBackgroundColor(ctx: Context): Int {
+        return reqAttrFromDevice(ctx, R.attr.colorBackground)
+    }
+
+    @ColorInt
+    private fun reqAttrFromDevice(ctx: Context, @AttrRes attr: Int): Int {
+        return MaterialColors.getColor(getCtx(ctx), attr, Color.MAGENTA)
+    }
+
+    private fun getCtx(ctx: Context) = ContextThemeWrapper(ctx, R.style.Theme_DeviceDefault_DayNight)
+
+}
