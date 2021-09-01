9b0641a825b67f40759664fc70e5426113ad05f9
Nick C <pokemonnekitgod@gmail.com>
2021-09-01 20:37:02 +0300

    bump version

--- TMessagesProj/src/main/java/ua/itaysonlab/extras/CatogramExtras.kt
+++ TMessagesProj/src/main/java/ua/itaysonlab/extras/CatogramExtras.kt
@@ -25,7 +25,7 @@ import java.lang.Exception
 
 object CatogramExtras {
 
-    var CG_VERSION = "3.8.9"
+    var CG_VERSION = "3.9.0"
     @JvmField
     var currentAccountBitmap: BitmapDrawable? = null
 
